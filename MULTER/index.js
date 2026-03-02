deb2da1c548fcaf4977554050327cf2c1e65e4fe
KaleruMadhu <2400030376@kluniversity.in>
2026-03-02 23:53:09 +0530

    Update index.js

--- MULTER/index.js
+++ MULTER/index.js
@@ -8,7 +8,7 @@ const PORT = 8000;
 console.log("INDEX FILE LOADED 🔥");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, './uploads'),
+    destination: (req, file, cb) => cb(null, path.resolve('./uploads')),
     filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
 });
 
